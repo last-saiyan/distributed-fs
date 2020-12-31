f9dd1f14eeb72261a515908a398101bf323e2133
nagesh <last1saiyan@gmail.com>
2020-12-31 23:05:00 +0530

    added config file for global constants

---
+++ utils/config.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// util for parsing configs json
+
+// Config contains configuration
+type Config struct {
+	BlockSize    int
+	NameNodePort int
+	DataNodePort int
+	Replica      int
+	EditLog      string
+	DataDir      string
+	NameNodeHost string
+}
+
+// GetConfig parses config.json and returns
+// Config struct
+func GetConfig() Config {
+	var config Config
+	file := "./config.json"
+	configFile, err := os.Open(file)
+	defer configFile.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	jsonParser := json.NewDecoder(configFile)
+	jsonParser.Decode(&config)
+	return config
+}
